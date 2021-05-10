d0dffbb66d03e77aa7bdeb5e174fedebb5532986
cnridley <cnridley1@gmail.com>
2021-05-10 00:18:13 +0000

    added typewriter to name and title

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
    // *** JS Typewriter Effect ***
-
    let i = 0;
    let target = document.getElementById("name");
+   let target1 = document.getElementById('name1');
    let text = target.innerHTML;
+   let text1 = target1.innerHTML;
    target.innerHTML = ' ';
-   let speed = 180; //speed duration of effect in millisec
+   target1.innerHTML = ' ';
+   let speed = 150; //speed duration of effect in millisec
+   var arrow = document.getElementById('arrowLink')
 
    typeWriter(); //to call function
    function typeWriter() {
-       if (i < text.length) {
+       if (i < text.length || text1.length) {
            target.innerHTML += text.charAt(i);
+           target1.innerHTML += text1.charAt(i);
            i++;
            setTimeout(typeWriter, speed);
        }
