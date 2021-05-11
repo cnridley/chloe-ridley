186e6c3e97fdf89a264438cb731326d7c5111362
cnridley <cnridley1@gmail.com>
2021-05-11 16:58:22 +0000

    made navbar sticky when it reaches the top

--- js/script.js
+++ js/script.js
@@ -23,12 +23,12 @@ window.addEventListener('load', function(){
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 1|| document.documentElement.scrollTop > 1000) {
-    document.getElementById("github").style.color = "white";
-    document.getElementById("linkedIn").style.color = "white";
-  } else {
+  if (document.body.scrollTop > 100|| document.documentElement.scrollTop > 100) {
     document.getElementById("github").style.color = "black";
     document.getElementById("linkedIn").style.color = "black";
+  } else {
+    document.getElementById("github").style.color = "white";
+    document.getElementById("linkedIn").style.color = "white";
   }
 }
 
